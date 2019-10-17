235f4b098e68ec37ec8bdcd9f438f2d8977d5563
Cássio <cassiolrm30@gmail.com>
2019-10-17 19:51:13 -0300

    Quinto commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace simulador_periodos_academicos.Controllers
@@ -24,6 +25,41 @@ namespace simulador_periodos_academicos.Controllers
         {
             ViewBag.Titulo = "Detalhes de Notas do Aluno";
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult ListarAlunos()
+        {
+            List<string> resultado = new List<string>();
+            resultado.Add("1|ALUNO 1");
+            resultado.Add("2|ALUNO 2");
+            return new JsonResult { Data = resultado, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpGet]
+        public ActionResult ListarMateriasPesosNotas(string alunoId)
+        {
+            List<string> resultado = new List<string>();
+            resultado.Add("1|MATÉRIA 1|1|1.5|2");
+            resultado.Add("2|MATÉRIA 2|1|2|1");
+            resultado.Add("3|MATÉRIA 3|1|1|2");
+            return new JsonResult { Data = resultado, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpGet]
+        public ActionResult ListarDetalhesNotasAluno(string alunoId)
+        {
+            List<string> resultado = new List<string>();
+            resultado.Add("ALUNO 1");
+            resultado.Add("101");
+            resultado.Add("MATEMÁTICA|1|10|1|10|1|10|1|10|blue|APROVADO");
+            resultado.Add("PORTUGUÊS|1|10|1|10|1|10|1|10|blue|APROVADO");
+            resultado.Add("FÍSICA|1|10|1|10|1|10|1|10|blue|APROVADO");
+            resultado.Add("QUÍMICA|1|10|1|10|1|10|1|10|blue|APROVADO");
+            resultado.Add("HISTÓRIA|1|10|1|10|1|10|1|10|red|REPROVADO");
+            resultado.Add("RELIGIÃO|1|10|1|10|1|10|1|10|blue|APROVADO");
+            resultado.Add("EDUCAÇÃO FÍSICA|1|10|1|10|1|10|1|10|blue|APROVADO");
+            return new JsonResult { Data = resultado, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
     }
 }
